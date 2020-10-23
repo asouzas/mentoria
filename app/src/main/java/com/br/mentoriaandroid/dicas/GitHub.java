34842bcfbb295d8a400355223531ec50526820b1
Aline Silva <aline.silva@picpay.com>
2020-10-23 14:08:37 -0300

    Dica GitHub

---
+++ app/src/main/java/com/br/mentoriaandroid/dicas/GitHub.java
@@ -0,0 +1,18 @@
+package com.br.mentoriaandroid.dicas;
+
+public class GitHub {
+
+    /**
+     * Principais comandos do Git, que será utilizado na mentoria
+     */
+
+    public GitHub() {
+
+        /*
+        * Para atualizar seu repositório local: git push
+        * Para salvar o código feito: git commit -m "digite a mensagem aqui"
+        * Para enviar seu código para o repositório remoto (após salvá-lo): git push
+        * Para descartar todas as informações que você fez: git hard reset
+        */
+    }
+}
