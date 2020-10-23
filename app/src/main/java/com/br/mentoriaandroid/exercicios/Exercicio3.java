4d38b7ecc0f431de2ba648c2510d337ae63b425c
Aline Silva <aline.silva@picpay.com>
2020-10-23 14:30:14 -0300

    Exercício 3

---
+++ app/src/main/java/com/br/mentoriaandroid/exercicios/Exercicio3.java
@@ -0,0 +1,38 @@
+package com.br.mentoriaandroid.exercicios;
+
+public class Exercicio3 {
+
+    /**
+     * Data da aula: 23/10/2020
+     * @author Aline Souza
+     *
+     * Vamos treinar um pouco dos comandos de GIT.
+     *
+     * Para resolver o exercício abaixo eu quero que:
+     * Commit a resolução do primeiro exercício com a mensagem "Exercício 1"
+     * Commit a resolução do segundo exercício com a mensagem "Exercício 2"
+     * Dê um PUSH nos dois de uma única vez
+     *
+     * Esqueceu os comandos? Olhar a classe GitHub, dentro da pasta "dicas"
+     */
+
+    public void getDaysFromAge(int age) {
+         /* Escreva um algorítmo que seja capaz de ler a idade passada por parâmetro e
+         * devolva uma mensagem da quantidade de dias correspondente.
+         * Exemplo: Se age == 10, então, o código deverá ter o resultado de 3650 dias.
+         * */
+
+        // Escreva seu código aqui.
+
+    }
+
+    public void getPreviousAndNext(int value) {
+        /*
+        * Desenvolva um algoritmo em Java que leia um número inteiro e
+        * caso ele seja positivo, armazene o seu antecessor, caso não, armazene seu sucessor.
+         */
+
+        // Escreva seu código aqui.
+
+    }
+}
